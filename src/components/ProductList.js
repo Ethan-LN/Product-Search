83d2306f92c46d386bcb2d8c3df77fd75e435441
Ethan <et.l.nicholas@gmail.com>
2023-06-29 13:31:05 +1000

    clean the codes

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -3,30 +3,35 @@ import { Product } from "./Product";
 import "./ProductList.css";
 import dataset from "../data/data.json";
 
-
 export const ProductList = () => {
-    const [products,setProducts] = useState(dataset)
+  const [products, setProducts] = useState(dataset);
 
-    const filterShirts = () => {
-        setProducts(dataset.filter((data) => {
-          return data.category === "Shirts"
-        }))
-      }
-      const filterPants = () => {
-        setProducts(dataset.filter((data) => {
-          return data.category === "Pants and Skirts"
-        }))
-      }
-      const filterJacket = () => {
-        setProducts(dataset.filter((data) => {
-          return data.category === "Jacket"
-        }))
-      }
+  const filterShirts = () => {
+    setProducts(
+      dataset.filter((data) => {
+        return data.category === "Shirts";
+      })
+    );
+  };
+  const filterPants = () => {
+    setProducts(
+      dataset.filter((data) => {
+        return data.category === "Pants and Skirts";
+      })
+    );
+  };
+  const filterJacket = () => {
+    setProducts(
+      dataset.filter((data) => {
+        return data.category === "Jacket";
+      })
+    );
+  };
 
-      const removeFilter = () => {
-        setProducts(dataset)
-      }
-    
+  const removeFilter = () => {
+    setProducts(dataset);
+  };
+
   return (
     <div className="products__panel">
       <div className="btns__group">
@@ -36,15 +41,15 @@ export const ProductList = () => {
         <button onClick={filterJacket}>Jacket</button>
       </div>
       <div className="cards">
-      {products.map((data) => (
-        <Product
-          className="item"
-          name={data.name}
-          image={data.imageURL}
-          desc={data.desc}
-          price={data.price}
-        />
-      ))}
+        {products.map((data) => (
+          <Product
+            className="item"
+            name={data.name}
+            image={data.imageURL}
+            desc={data.desc}
+            price={data.price}
+          />
+        ))}
       </div>
     </div>
   );
